4e8eadee9795b218a99b7597d3c9031407d3fed8
peterGeswint <petergeswint@outlook.com>
2024-11-28 09:54:28 +0000

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,18 +3,19 @@
 Change comes from within
 """
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     coin_count = 0
-    
+
     for coin in coins:
         while total >= coin:
             total -= coin
             coin_count += 1
         if total == 0:
             return coin_count
-    
-    return -1
+
+        return -1
