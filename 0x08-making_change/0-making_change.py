97aa6ef39bccd45cc6fcef4c70d0805fa445bd1d
peterGeswint <petergeswint@outlook.com>
2024-11-28 09:47:26 +0000

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    
+    coins.sort(reverse=True)
+    coin_count = 0
+    
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            coin_count += 1
+        if total == 0:
+            return coin_count
+    
+    return -1
