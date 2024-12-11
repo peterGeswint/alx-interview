501a2dffd3a7cc10c3381f49f398b73298f11e7c
peterGeswint <petergeswint@outlook.com>
2024-12-11 18:50:34 +0000

    task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,40 +1,55 @@
 #!/usr/bin/python3
+def sieve_of_eratosthenes(max_n):
+    """Generate a list of primes up to max_n using the Sieve of Eratosthenes."""
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+    for i in range(2, int(max_n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
 
-def get_primes_up_to(n):
     primes = []
-    for num in range(2, n + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
+    for i in range(max_n + 1):
+        if is_prime[i]:
+            primes.append(i)
+
+    return primes, is_prime
+
+
+def count_moves(n, primes, is_prime):
+    """Count the number of moves possible for a given n."""
+    moves = 0
+    taken = [False] * (n + 1)
+
+    for prime in primes:
+        if prime > n:
+            break
+        if not taken[prime]:
+            moves += 1
+            for multiple in range(prime, n + 1, prime):
+                taken[multiple] = True
+
+    return moves
+
 
 def isWinner(x, nums):
-    if not nums or x < 1:
+    """Determine the winner of the game."""
+    if not nums or x <= 0:
         return None
+
+    max_n = max(nums)
+    primes, is_prime = sieve_of_eratosthenes(max_n)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes = get_primes_up_to(n)
-        move_count = 0
-
-        while primes:
-            prime = primes.pop(0)
-            move_count += 1
-            primes = [p for p in primes if p % prime != 0]
-
-        if move_count % 2 == 0:
-            ben_wins += 1
-        else:
+        moves = count_moves(n, primes, is_prime)
+        if moves % 2 == 1:  # Maria wins
             maria_wins += 1
+        else:  # Ben wins
+            ben_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
@@ -42,6 +57,7 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
 
 if __name__ == "__main__":
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
